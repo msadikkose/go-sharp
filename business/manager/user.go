11adfa40074a83ac49b3c30cad1f55a512111bfe
muhammedsadik <sadik@sensusedu.com>
2020-08-31 16:01:51 +0300

    code cleaning

--- business/manager/user.go
+++ business/manager/user.go
@@ -5,26 +5,6 @@ import (
 	"go-sharp/model"
 )
 
-type TrainData struct {
-	sentence string
-	sentiment string
-	weight int
-}
-
-var TrainDataCity = []TrainData {
-	{"I love the weather here.", "pos", 1700},
-	{"This is an amazing place!", "pos", 2000},
-	{"I feel very good about its food and atmosphere.", "pos", 2000},
-	{"The location is very accessible.", "pos", 1500},
-	{"One of the best cities I've ever been.", "pos", 2000},
-	{"Definitely want to visit again.", "pos", 2000},
-	{"I do not like this area.", "neg", 500},
-	{"I am tired of this city.", "neg", 700},
-	{"I can't deal with this town anymore.", "neg", 300},
-	{"The weather is terrible.", "neg", 300},
-	{"I hate this city.", "neg", 100},
-	{"I won't come back!", "neg", 200},
-}
 
 type IUser interface {
 	GetById(idUser int) (*model.User, error)
