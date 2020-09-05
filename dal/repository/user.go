5f01eee2bd13be2cc123e327da4262dca07c4a1a
muhammedsadik <sadik@sensusedu.com>
2020-09-06 02:28:21 +0300

    Removed unnecessary go func and sync usages

--- dal/repository/user.go
+++ dal/repository/user.go
@@ -4,14 +4,13 @@ import (
 	"go-sharp/ent"
 	"go-sharp/ent/user"
 	"go-sharp/model"
-	"sync"
 )
 
 type IUser interface {
 	Get(idUser int) (*ent.User, error)
 	GetAll() ([]*ent.User, error)
 	Create(userEnt *model.User) error
-	Delete(idUser int)  error
+	Delete(idUser int) error
 	Update(usr *model.User) error
 }
 
@@ -22,14 +21,9 @@ type User struct {
 func (u *User) Get(idUser int) (*ent.User, error) {
 	var usr *ent.User
 	var err error
-	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		usr, err = u.Transaction.User.Query().Where(user.IDEQ(idUser)).Where(user.IsDeleted(false)).Only(u.Ctx)
-	}()
-	wg.Wait()
+	usr, err = u.Transaction.User.Query().Where(user.IDEQ(idUser)).Where(user.IsDeleted(false)).Only(u.Ctx)
+
 	if err != nil {
 		return nil, err
 	}
@@ -40,12 +34,9 @@ func (u *User) GetAll() ([]*ent.User, error) {
 
 	var usr []*ent.User
 	var err error
-	wg := make(chan int)
-	go func() {
-		usr, err = u.Transaction.User.Query().Where(user.IsDeleted(false)).Order(ent.Asc(user.FieldID)).All(u.Ctx)
-		wg <- 1
-	}()
-	<-wg
+
+	usr, err = u.Transaction.User.Query().Where(user.IsDeleted(false)).Order(ent.Asc(user.FieldID)).All(u.Ctx)
+
 	if err != nil {
 		return nil, err
 	}
@@ -53,20 +44,15 @@ func (u *User) GetAll() ([]*ent.User, error) {
 }
 func (u *User) Create(userModel *model.User) error {
 	var err error
-	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		_, err = u.Transaction.User.
-			Create().
-			SetAge(userModel.Age).
-			SetName(userModel.Name).
-			SetIsDeleted(false).
-			Save(u.Ctx)
-		defer wg.Done()
-	}()
-	wg.Wait()
-	if err !=nil{
+	_, err = u.Transaction.User.
+		Create().
+		SetAge(userModel.Age).
+		SetName(userModel.Name).
+		SetIsDeleted(false).
+		Save(u.Ctx)
+
+	if err != nil {
 		return err
 	}
 	return nil
@@ -74,45 +60,34 @@ func (u *User) Create(userModel *model.User) error {
 
 func (u *User) Update(userModel *model.User) error {
 	var err error
-	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		if(userModel.Id>0) {
-			_, err = u.Transaction.User.
-				UpdateOneID(userModel.Id).
-				SetAge(userModel.Age).
-				SetName(userModel.Name).
-				SetIsDeleted(false).
-				Save(u.Ctx)
-		}
-		defer wg.Done()
-	}()
-	wg.Wait()
-	if err != nil{
+	if userModel.Id > 0 {
+		_, err = u.Transaction.User.
+			UpdateOneID(userModel.Id).
+			SetAge(userModel.Age).
+			SetName(userModel.Name).
+			SetIsDeleted(false).
+			Save(u.Ctx)
+	}
+
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (u *User) Delete(idUser int)  error {
+func (u *User) Delete(idUser int) error {
 
 	var err error
-	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		_, err = u.Transaction.User.
-			UpdateOneID(idUser).
-			SetIsDeleted(true).
-			Save(u.Ctx)
+	_, err = u.Transaction.User.
+		UpdateOneID(idUser).
+		SetIsDeleted(true).
+		Save(u.Ctx)
 
-	}()
-	wg.Wait()
 	if err != nil {
 		return nil
 	}
 
-	return  nil
+	return nil
 }
